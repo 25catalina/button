8a41d6774292d138af06ced221ffc7efdf5699fc
catalina <catalina.caripan.lonconanco@alumnos.sip.cl>
2025-10-28 10:06:55 -0300

    palta

---
+++ caja.js
@@ -0,0 +1,8 @@
+function btnFunc1 () {
+    let btn = document.querySelector (".login-btn");
+    btn.innerText = "Sesion cerrada";
+}
+function btnFunc2 () {
+    let btnText = document.querySelector ("#btn-text-1");
+    btnText.innerText = parseInt (btnText.innerText) + 1;
+}
